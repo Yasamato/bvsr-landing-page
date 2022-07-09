f9702947b661e83e4c40243e33f69cd98676268a
Yasamato <agent77326@gmail.com>
2022-07-09 18:15:09 +0200

    Allow data blobs for image

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: 'default-src \'self\'; style-src \'self\' \'unsafe-inline\';'
+                        value: 'default-src \'self\'; style-src \'self\' \'unsafe-inline\'; img-src \'self\' data:;'
                     }
                 ]
             }
