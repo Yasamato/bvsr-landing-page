9b5e4c8fe4bc00ed1f13170ddf112556dde3a5ed
Yasamato <agent77326@gmail.com>
2022-07-09 18:10:25 +0200

    Allow inline css

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: 'default-src \'self\';'
+                        value: 'default-src \'self\'; style-src \'self\' \'unsafe-inline\';'
                     }
                 ]
             }
