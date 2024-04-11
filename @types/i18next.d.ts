d336f9edfd22cac898b679371975a58ca01cf503
Yasamato <agent77326@gmail.com>
2024-04-11 09:13:02 +0200

    Update i18next.d.ts

--- @types/i18next.d.ts
+++ @types/i18next.d.ts
@@ -5,23 +5,25 @@
  *
  * @link https://www.i18next.com/overview/typescript
  */
-import 'i18next'
+import "i18next";
 
-import type common from '../public/locales/de/common.json'
-import type footer from '../public/locales/de/footer.json'
-import type members from '../public/locales/de/members.json'
-import type navbar from "../public/locales/de/navbar.json"
+import type common from "../public/locales/de/common.json";
+import type footer from "../public/locales/de/footer.json";
+import type members from "../public/locales/de/members.json";
+import type navbar from "../public/locales/de/navbar.json";
+import type sponsors from "../public/locales/de/sponsors.json";
 
 interface I18nNamespaces {
-    common: typeof common
-    footer: typeof footer
-    members: typeof members
-    navbar: typeof navbar
+  common: typeof common;
+  footer: typeof footer;
+  members: typeof members;
+  navbar: typeof navbar;
+  sponsors: typeof sponsors;
 }
 
-declare module 'i18next' {
-    interface CustomTypeOptions {
-        defaultNS: 'common'
-        resources: I18nNamespaces
-    }
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: "common";
+    resources: I18nNamespaces;
+  }
 }
